0e126c798c323df6bcb33252751923aff3b8d2e5
Jake Atwell <jake.atwell@gmail.com>
2024-12-20 10:53:26 -0500

    Put an example recipe in a data.py file

--- api/tests.py
+++ api/tests.py
@@ -1,101 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Recipe, Task
-
-
-"""
-Tasks:
-========
-"get out all ingredients",
-"get out cutting board and knife",
-"get out pot",
-"get out measuring cups and spoons",
-"put pot on stove and turn heat to medium/low",
-"chop onions",
-"put oil in pot",
-"put onions in oiled pot",
-"stir onions & sautee for ~10 mninutes until golden",
-"get out a bowl for the spices",
-"measure out all the spices, put into a bowl",
-"add spices to pot with sauteed onions and mix",
-"dice/plane garlic",
-"stir garlic into onions and sautee for ~2 minutes",
-"measure out 2 cups vegetable broth (or make it from boullion)",
-"get out can opener",
-"open can of crushed tomatoes",
-"add broth to pot",
-"add water to pot",
-"add tomatoes to pot",
-"chop carrots",
-"add carrots to pot",
-"bring pot to a boil",
-"reduce pot heat and keep at a simmer",
-"add one cup of uncooked lentils to pot",
-"open can of chickpeas and add to pot",
-"get out wooden spoon",
-"give pot a deep stir, scraping everything off the bottom with a wooden spoon",
-"squeeze lemon juice onto stew",
-"turn off heat",
-"get out bowls",
-"get out ladle for serving stew",
-"get out yogurt and spoon",
-"serve stew into bowls",
-"distribute leftovers evenly into everybody's takehome containers",
-"eat",
-"everybody should bring all dirty dishes to near the sink",
-"wash dishes for 5 minutes",
-"dry dishes and put them away",
-
-
-Ingredients:
-============
-"""
+from .data import test_recipe
 
 
 class IndexViewTests(TestCase):
     def setUp(self):
-        self.tasks = [
-            "get out all ingredients",
-            "get out cutting board and knife",
-            "get out pot",
-            "get out measuring cups and spoons",
-            "put pot on stove and turn heat to medium/low",
-            "chop onions",
-            "put oil in pot",
-            "put onions in oiled pot",
-            "stir onions & sautee for ~10 mninutes until golden",
-            "get out a bowl for the spices",
-            "measure out all the spices, put into a bowl",
-            "add spices to pot with sauteed onions and mix",
-            "dice/plane garlic",
-            "stir garlic into onions and sautee for ~2 minutes",
-            "measure out 2 cups vegetable broth (or make it from boullion)",
-            "get out can opener",
-            "open can of crushed tomatoes",
-            "add broth to pot",
-            "add water to pot",
-            "add tomatoes to pot",
-            "chop carrots",
-            "add carrots to pot",
-            "bring pot to a boil",
-            "reduce pot heat and keep at a simmer",
-            "add one cup of uncooked lentils to pot",
-            "open can of chickpeas and add to pot",
-            "get out wooden spoon",
-            "give pot a deep stir, scraping everything off the bottom with a wooden spoon",
-            "squeeze lemon juice onto stew",
-            "turn off heat",
-            "get out bowls",
-            "get out ladle for serving stew",
-            "get out yogurt and spoon",
-            "serve stew into bowls",
-            "distribute leftovers evenly into everybody's takehome containers",
-            "eat",
-            "everybody should bring all dirty dishes to near the sink",
-            "wash dishes for 5 minutes",
-            "dry dishes and put them away",
-        ]
+        self.tasks = test_recipe["tasks"]
+        self.recipe_name = test_recipe["name"]
 
     def test_index_view(self):
         response = self.client.get(reverse("index"))
@@ -103,7 +15,7 @@ class IndexViewTests(TestCase):
         self.assertContains(response, "Hello, world. You're at the polls index.")
 
     def test_create_recipe(self):
-        obj = Recipe.objects.create(name="Lentil stew")
+        obj = Recipe.objects.create(name=self.recipe_name)
         recipe_id = obj.id
 
         task_objs = [Task(description=task, recipe_id=recipe_id) for task in self.tasks]
