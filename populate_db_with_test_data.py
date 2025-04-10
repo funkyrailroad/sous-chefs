5b746aa149cb0c6471cdd930a79dfc696bf6c70a
Jake Atwell <jake.atwell@gmail.com>
2025-04-10 10:23:59 -0400

    Create test admin and regular users

--- populate_db_with_test_data.py
+++ populate_db_with_test_data.py
@@ -1,4 +1,23 @@
+from django.contrib.auth import get_user_model
 from my_app.tests import create_test_recipe
+
+User = get_user_model()
+
+# Admin user
+user = User.objects.create_superuser(
+    username="myadmin",
+    email="admin@example.com",
+    password="mypassword",
+)
+user.save()
+
+# Non-admin user
+user = User.objects.create_user(
+    username="myuser",
+    email="user@example.com",
+    password="mypassword",
+)
+user.save()
 
 
 create_test_recipe()
